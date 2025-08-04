289c085b3b67235bab5bbddfa1778da8233b2f7d
Aras Abbasi <aras.abbasi@googlemail.com>
2025-08-04 12:06:49 +0200

    implement get-week-of-year, add some basics

---
+++ test/get-week-of-year.test.js
@@ -0,0 +1,14 @@
+'use strict'
+
+const { describe, it } = require('node:test')
+const weekOfYear = require('./fixtures/week-of-year.json')
+const getWeekOfYear = require('../lib/get-week-of-year')
+
+describe('getWeekOfYear', () => {
+  it('should return the correct week of year', (t) => {
+    for (const [date, expected] of weekOfYear) {
+      const d = new Date(date)
+      t.assert.strictEqual(getWeekOfYear(d), expected, `Week of year for ${date} should be ${expected}`)
+    }
+  })
+})
